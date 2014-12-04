21aabf4dbcaea639de6c60c6e36470cab0ca8d37
Alex Wilson <alex@cooperi.net>
2014-12-04 15:37:10 +1000

    Handle directories of dependant mibs better

--- src/snmerp_mib.erl
+++ src/snmerp_mib.erl
@@ -106,6 +106,68 @@ oid_to_me(Oid, #mibdat{oid2me = Oid2Me}) ->
 		_ -> not_found
 	end.
 
+-spec add_names_enums(#mib{}, mibset()) -> mibset().
+add_names_enums(#mib{mes = Mes}, D = #mibdat{}) ->
+	lists:foldl(fun
+		(Me = #me{entrytype = Type}, DD = #mibdat{})
+				when (Type =:= table) or (Type =:= table_column) or (Type =:= variable) ->
+			#me{aliasname = NameAtom, oid = Oid, asn1_type = Asn1Type} = Me,
+			#mibdat{name2oid = Name2Oid, oid2me = Oid2Me, enums = Enums} = DD,
+			Name2Oid2 = trie:store(atom_to_list(NameAtom), Oid, Name2Oid),
+			Oid2Me2 = trie:store(Oid, Me, Oid2Me),
+			Enums2 = case Asn1Type of
+				#asn1_type{} ->
+					case proplists:get_value(enums, Asn1Type#asn1_type.assocList) of
+						undefined -> Enums;
+						RawEnum ->
+							Enum = [{V, K} || {K,V} <- RawEnum],
+							trie:store(Oid, Enum, Enums)
+					end;
+				_ -> Enums
+			end,
+			DD#mibdat{name2oid = Name2Oid2, oid2me = Oid2Me2, enums = Enums2};
+		(_, DD) -> DD
+	end, D, Mes).
+
+-spec add_table_entries(#mib{}, mibset()) -> mibset().
+add_table_entries(#mib{mes = Mes}, D = #mibdat{}) ->
+	lists:foldl(fun
+		(Me = #me{entrytype = table_entry}, DD = #mibdat{}) ->
+			#me{aliasname = NameAtom, mfa = MFA} = Me,
+			#mibdat{name2oid = Name2Oid} = DD,
+			{snmp_generic,table_func,[{TableAtom,_}]} = MFA,
+			TableOid = trie:fetch(atom_to_list(TableAtom), Name2Oid),
+			Name2Oid2 = trie:store(atom_to_list(NameAtom), TableOid, Name2Oid),
+			DD#mibdat{name2oid = Name2Oid2};
+		(_, DD) -> DD
+	end, D, Mes).
+
+-spec add_table_columns(#mib{}, mibset()) -> mibset().
+add_table_columns(#mib{mes = Mes}, D = #mibdat{}) ->
+	lists:foldl(fun
+		(Me = #me{entrytype = Type}, DD = #mibdat{})
+				when (Type =:= table) or (Type =:= table_column) ->
+			#me{oid = Oid, assocList = Assocs} = Me,
+			#mibdat{name2oid = Name2Oid, tables = Tbls} = DD,
+			Tbls2 = case proplists:get_value(table_name, Assocs) of
+				undefined when (Type =:= table) ->
+					TableInfo = proplists:get_value(table_info, Assocs),
+					TTbls = case TableInfo#table_info.index_types of
+						{augments, {ParentTblAtom, _}} ->
+							ParentOid = trie:fetch(atom_to_list(ParentTblAtom), Name2Oid),
+							trie:append(ParentOid, Me, Tbls);
+						_ -> Tbls
+					end,
+					trie:append(Oid, Me, TTbls);
+				undefined -> Tbls;
+				TblNameAtom ->
+					TblOid = trie:fetch(atom_to_list(TblNameAtom), Name2Oid),
+					trie:append(TblOid, Me, Tbls)
+			end,
+			DD#mibdat{tables = Tbls2};
+		(_, DD) -> DD
+	end, D, Mes).
+
 %% @doc Adds a .bin file to a MIB set.
 -spec add_file(Path :: string(), mibset()) -> {ok, mibset()} | {error, Reason :: term()}.
 add_file(Path, D = #mibdat{}) ->
@@ -113,57 +175,11 @@ add_file(Path, D = #mibdat{}) ->
 		{ok, Bin} ->
 			case (catch binary_to_term(Bin)) of
 				{'EXIT', Reason} -> {error, Reason};
-				#mib{mes = Mes} ->
-					D2 = lists:foldl(fun
-						(Me = #me{entrytype = table_entry}, DD = #mibdat{}) ->
-							#me{aliasname = NameAtom, mfa = MFA} = Me,
-							#mibdat{name2oid = Name2Oid} = DD,
-							{snmp_generic,table_func,[{TableAtom,_}]} = MFA,
-							TableOid = trie:fetch(atom_to_list(TableAtom), Name2Oid),
-							Name2Oid2 = trie:store(atom_to_list(NameAtom), TableOid, Name2Oid),
-							DD#mibdat{name2oid = Name2Oid2};
-
-						(Me = #me{entrytype = EntType}, DD = #mibdat{})
-								when (EntType =:= variable) or (EntType =:= table_column)
-								or (EntType =:= table) ->
-							#me{oid = Oid, aliasname = NameAtom,
-								asn1_type = Asn1Type, assocList = Assocs} = Me,
-							#mibdat{name2oid = Name2Oid, oid2me = Oid2Me,
-								enums = Enums, tables = Tbls} = DD,
-							Name2Oid2 = trie:store(atom_to_list(NameAtom), Oid, Name2Oid),
-							Oid2Me2 = trie:store(Oid, Me, Oid2Me),
-							Enums2 = case Asn1Type of
-								#asn1_type{} ->
-									case proplists:get_value(enums, Asn1Type#asn1_type.assocList) of
-										undefined -> Enums;
-										RawEnum ->
-											Enum = [{V, K} || {K,V} <- RawEnum],
-											trie:store(Oid, Enum, Enums)
-									end;
-								_ -> Enums
-							end,
-							Tbls2 = case proplists:get_value(table_name, Assocs) of
-								undefined when (EntType =:= table) ->
-									TableInfo = proplists:get_value(table_info, Assocs),
-									TTbls = case TableInfo#table_info.index_types of
-										{augments, {ParentTblAtom, _}} ->
-											ParentOid = trie:fetch(atom_to_list(ParentTblAtom), Name2Oid),
-											trie:append(ParentOid, Me, Tbls);
-										_ -> Tbls
-									end,
-									trie:append(Oid, Me, TTbls);
-								undefined -> Tbls;
-								TblNameAtom ->
-									TblOid = trie:fetch(atom_to_list(TblNameAtom), Name2Oid),
-									trie:append(TblOid, Me, Tbls)
-							end,
-							DD#mibdat{name2oid = Name2Oid2, oid2me = Oid2Me2,
-								enums = Enums2, tables = Tbls2};
-
-						(#me{}, DD = #mibdat{}) ->
-							DD
-					end, D, Mes),
-					{ok, D2}
+				Mib = #mib{} ->
+					D2 = add_names_enums(Mib, D),
+					D3 = add_table_entries(Mib, D2),
+					D4 = add_table_columns(Mib, D3),
+					{ok, D4}
 			end;
 		{error, Reason} -> {error, Reason}
 	end.
@@ -173,16 +189,23 @@ add_file(Path, D = #mibdat{}) ->
 add_dir(Path, D = #mibdat{}) ->
 	case file:list_dir(Path) of
 		{ok, Fnames} ->
-			D2 = lists:foldl(fun(Fn, DD) ->
+			Mibs = lists:foldl(fun(Fn, Acc) ->
 				case lists:suffix(".bin", Fn) of
 					true ->
-						case add_file(filename:join([Path, Fn]), DD) of
-							{ok, DD2} -> DD2;
-							_ -> DD
+						case file:read_file(filename:join([Path, Fn])) of
+							{ok, Bin} ->
+								case (catch binary_to_term(Bin)) of
+									{'EXIT', _Reason} -> Acc;
+									Mib = #mib{} -> [Mib | Acc]
+								end;
+							_ -> Acc
 						end;
-					false -> DD
+					false -> Acc
 				end
-			end, D, Fnames),
-			{ok, D2};
+			end, [], Fnames),
+			D2 = lists:foldl(fun add_names_enums/2, D, Mibs),
+			D3 = lists:foldl(fun add_table_entries/2, D2, Mibs),
+			D4 = lists:foldl(fun add_table_columns/2, D3, Mibs),
+			{ok, D4};
 		{error, Reason} -> {error, Reason}
 	end.
